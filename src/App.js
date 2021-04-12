78180ff49f58155b746bd0a71a6fe78c621430dc
parkgunyoung <keonyeong@gmail.com>
2021-04-12 22:28:06 +0900

    0412-3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import './App.css';
+
+
+
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'world wide web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        Hello, React!!
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
